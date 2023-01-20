2f63f6309152494c19643ab746df01c20cba45bb
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-20 07:40:43 +0000

    changes in statesArray

--- src/components/App.js
+++ src/components/App.js
@@ -3,194 +3,38 @@ import "./../styles/App.css";
 
 // Do not alter the states const and values inside it.
 const states = [
-  {
-    name: "Madhya Pradesh",
-    cities: [
-      {
-        name: "Indore",
-        towns: [
-          {
-            name: "Mhow",
-          },
-          {
-            name: "Dewas",
-          },
-        ],
-      },
-      {
-        name: "Bhopal",
-        towns: [
-          {
-            name: "Manit",
-          },
-          {
-            name: "Berasia",
-          },
-        ],
-      },
-      {
-        name: "Gwalior",
-        towns: [
-          {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
+  {name: "Madhya Pradesh",
+    cities: [{ name: "Indore", towns: [{name: "Mhow"},{name: "Dewas"}]},
+            {name: "Bhopal", towns: [{name: "Manit"},{name: "Berasia"}]},
+            {name: "Gwalior", towns: [{name: "Ajaypur"}]}]
   },
-  {
-    name: "Jharkhand",
-    cities: [
-      {
-        name: "Dhanbad",
-        towns: [
-          {
-            name: "IIT(ISM) Dhanbad",
-          },
-          {
-            name: "Hirapur",
-          },
-        ],
-      },
-      {
-        name: "Wasseypur",
-        towns: [
-          {
-            name: "Sardar khan's",
-          },
-          {
-            name: "Faizal khan's",
-          },
-        ],
-      },
-      {
-        name: "Mirzapur",
-        towns: [
-          {
-            name: "Kaleen bhaiya's",
-          },
-          {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Assam",
-    cities: [
-      {
-        name: "Guwhati",
-        towns: [
-          {
-            name: "Amin",
-          },
-          {
-            name: "Jalah",
-          },
-        ],
-      },
-      {
-        name: "Jungle1",
-        towns: [
-          {
-            name: "Tiger found at IIT Guwahati",
-          },
-          {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
-      },
-      {
-        name: "Tezpur",
-        towns: [
-          {
-            name: "Dibrugarh",
-          },
-          {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Bihar",
-    cities: [
-      {
-        name: "Patna",
-        towns: [
-          {
-            name: "Sonpur",
-          },
-          {
-            name: "Maner",
-          },
-        ],
-      },
-      {
-        name: "Gaya",
-        towns: [
-          {
-            name: "Bakraur",
-          },
-          {
-            name: "Barachatti",
-          },
-        ],
-      },
-      {
-        name: "Darbhanga",
-        towns: [
-          {
-            name: "Singhwara",
-          },
-          {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
+  {name: "Jharkhand",
+    cities: [{name: "Dhanbad", towns: [{name: "IIT(ISM) Dhanbad"},{name: "Hirapur"}]},
+              {name: "Wasseypur", towns: [{name: "Sardar khan's"},{name: "Faizal khan's"}]},
+              {name: "Mirzapur",towns: [{name: "Kaleen bhaiya's"},{name: "Guddu bhaiya's"}]},
+    ]},
+  {name: "Assam",
+    cities: [{name: "Guwhati", towns: [{name: "Amin"},{name: "Jalah"}]},
+      {name: "Jungle1", towns: [{name: "Tiger found at IIT Guwahati"},{name: "Leopard found in IIT Guwahati"}]},
+      {name: "Tezpur", towns: [{name: "Dibrugarh"},{name: "Silchar"}]},
+    ]},
+  {name: "Bihar",
+    cities: [{name: "Patna", towns: [{name: "Sonpur"},{name: "Maner"}]},
+      {name: "Gaya", towns: [{name: "Bakraur"},{name: "Barachatti"}]},
+      {name: "Darbhanga", towns: [{name: "Singhwara"},{name: "Jale"}]},
+    ]},
 ];
 
 function App() {
 
   let stateCount = 1;
-  let cityCount = 1;
 
   const [stateName, setStateName] = useState('none');
   const [isSelectedState, setIsSeletedState] = useState(false);
 
-  const [cityName, setCityName] = useState('none');
-  const [isCitySelected, setIsCitySelected] = useState(false);
-
   function selectState (event) {
     setStateName(event.target.value);
     setIsSeletedState(true);
-  }
-
-  function changeCity (event) {
-    setCityName(event.target.value);
-    setIsCitySelected(true);
-  }
-
-  const Selectcity = () => {
-
-    return (
-      <>
-        <select name="cities" id="cities" value={cityName} onChange={changeCity}> 
-           <option value={'none'}  disabled key={'none'}>--select city--</option>
-          {states.map((obj) => {
-            if(obj.name === stateName){
-              obj.cities.map((city) => {
-                return <option value={`${city.name}`} key={`state${stateCount}`} id={`state${stateCount++}`}>{city.name}</option>
-              })
-            }
-          })}
-        </select>
-      </>
-    )
   }
 
   return (
@@ -201,9 +45,7 @@ function App() {
           return <option value={`${obj.name}`} key={`state${stateCount}`} id={`state${stateCount++}`}>{obj.name}</option>
         })}
       </select>
-      {isSelectedState && <Selectcity />}
-      
-
+      {/* {isSelectedState && <Selectcity cities={states[stateName]}/>} */}
     </div>
   );
 }
